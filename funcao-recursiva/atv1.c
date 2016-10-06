06fc3f3892c13a67770466da5ba78d305d6d829a
icaroX10 <icaro_music@hotmail.com>
2016-10-06 14:01:12 -0300

    Create atv1.c

---
+++ funcao-recursiva/atv1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int fib(int n){
+	if((n == 1)||(n == 2)){
+		return 1;
+	}else{
+		return fib(n-1)+fib(n-2);;
+	}
+}
+int main(void){
+	int x;
+	printf("Digite o tamanho:");
+	scanf("%d",&x);
+	printf("%d",fib(x));
+	return 0;
+}

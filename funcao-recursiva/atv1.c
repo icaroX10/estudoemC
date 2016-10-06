b0f250cbd34c8e2e26fa204081868821bc5bbd19
icaroX10 <icaro_music@hotmail.com>
2016-10-06 14:31:58 -0300

    Update atv1.c

--- funcao-recursiva/atv1.c
+++ funcao-recursiva/atv1.c
@@ -13,3 +13,21 @@ int main(void){
 	printf("%d",fib(x));
 	return 0;
 }
+//Estilo Berto-----
+
+#include <stdio.h>
+int fib(int pos,int act,int ant){
+	printf(" %d ",ant);
+	if(pos <= 1){
+		return;
+	}else{
+		return fib(pos-1,act+ant,act);
+	}
+}
+int main(void){
+	int x;
+	printf("Digite o tamanho:");
+	scanf("%d",&x);
+	fib(x,1,0);
+	return 0;
+}

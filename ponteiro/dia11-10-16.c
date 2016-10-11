f035d2d573ffbaa856d7cfcb5951fc18b4ecef27
icaroX10 <icaro_music@hotmail.com>
2016-10-11 14:42:53 -0300

    Create dia11-10-16.c

---
+++ ponteiro/dia11-10-16.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int teste(){
+	int x = 5;
+	char c[1];
+	int *pi;
+	c[0] = 'g';
+	char *pc;
+	pi = &x;
+	pc = &c;
+	printf("%d\n",pi);
+	//Precisa ta em %d pra aparecer o valor da memoria
+	printf("%d\n",*pc);
+	//Com o * você pegara o valor que esta contido na memoria referenciada
+	printf("%c\n",*pc);
+	
+}
+void test2(){
+	int x;
+	// x nesse momento esta recebendo o valor 10
+	x = 10;
+	// Aqui to criando o ponteiro
+	int *ponteiro;
+	// o ponteiro esta recebendo o endereço da memoria de x
+	ponteiro = &x;
+	
+	int y = 20;
+	// Aqui o valor do ponteiro que esta referenciando a memoria de x vai receber 20... ou seja... x de 10 vai pra 20
+	*ponteiro = y;
+	
+	printf("Valor do X = %d\nValor do Y = %d\nValor do ponteiro = %d\n",x,y,ponteiro);
+	return 0;
+}
+main(){
+	test2();
+}

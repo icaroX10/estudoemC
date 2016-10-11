8d68bd19e0127f3134c7cebcb5cd8aa42781c0be
icaroX10 <icaro_music@hotmail.com>
2016-10-11 16:28:44 -0300

    Mais um exercicio

--- ponteiro/ex4.c
+++ ponteiro/ex4.c
@@ -8,6 +8,15 @@ void vetore(){
 	printf("\n %c \n", *point);
 	getch();
 }
+void calcV(){
+	int mat[10] = {1,8,3,7,5,3,7,23,68,10};
+	int *point, i;
+	
+	point = mat;
+	for(i=0;i<10;i++){
+		printf("%d\n",*(point+i));
+	}
+}
 main(){
-	vetorCmais();
+	calcV();
 }
